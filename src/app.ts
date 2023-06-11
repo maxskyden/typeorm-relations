083c3a8ea440ac94cc611d6034fc2f2275af2c72
Netto Querois <24768847+nettoquerois@users.noreply.github.com>
2023-06-11 18:30:30 -0300

    Create base path v1 to manage versioning with Swagger

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
+app.use("v1", routes);
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
